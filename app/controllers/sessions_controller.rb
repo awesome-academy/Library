991ac76d82ba1b66aebc7c6d4283e20a97b74a67
Hong Son <tnhs97@gmail.com>
2019-06-13 13:54:02 +0700

    signup

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,34 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    @user = User.find_by email: params[:session][:email].downcase
+    if @user&.authenticate params[:session][:password]
+      login_and_redirect @user
+    else
+      flash.now[:danger] = t "misc.login_fails"
+      render :new
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_path
+  end
+
+  private
+
+  def remmember? user
+    if params[:session][:remember_me] == Settings.remember_me
+      remember user
+    else
+      forget user
+    end
+  end
+
+  def login_and_redirect user
+    log_in user
+    remmember? user
+    redirect_back_or user
+  end
+end
